a1b3e9246f77380bf87b7d7834027886b9888001
Falgor.C <tranhoa.7604@gmail.com>
2021-04-20 17:45:25 +0700

    display entries

--- src/component/ListEntry.vue
+++ src/component/ListEntry.vue
@@ -2,8 +2,11 @@
   <v-main data-testid="listentry">
     <v-container fluid>
       <v-row align="center">
-        <v-col align="center">
-          <div>ListEntry</div>
+        <v-col align="center" v-for="entry in entries">
+          <v-card data-testid="entryCard">
+            <v-card-title>{{entry.title}}</v-card-title>
+            <v-card-text>{{entry.content}}</v-card-text>
+          </v-card>
         </v-col>
       </v-row>
       <v-btn absolute bottom right fab @click="createEntry" data-testid="createButton">
@@ -13,13 +16,17 @@
   </v-main>
 </template>
 <script>
+  import { mapState } from 'vuex';
   import { UnauthenticatedException } from '../api';
+
   export default {
     methods: {
       createEntry() {
         this.$router.push('/entry/create');
       }
     },
+
+    computed: mapState(['entries']),
 
     async mounted() {
       try {
--- test/component/listentry.test.js
+++ test/component/listentry.test.js
@@ -1,8 +1,22 @@
+import { waitFor } from '@testing-library/vue';
+
 import { render } from '../util.js';
 import ListEntry from '../../src/component/ListEntry';
+import api from '../../src/api';
 
 describe('component/ListEntry', () => {
   test('list successfully', async () => {
-    const { getByTestId, findByText } = render(ListEntry);
+    api.setToken('token1');
+    let mockFetch = jest.fn();
+    mockFetch.mockReturnValue(Promise.resolve({
+      status: 200,
+      json: () => Promise.resolve([{id: 1, title: 'title 1', content: 'content 1'}, {id: 2, title: 'title 2', content: 'content 2'}]),
+    }));
+    global.fetch = mockFetch;
+
+    const { getAllByTestId, findByTestId } = render(ListEntry);
+    await waitFor(() => expect(document.querySelector('.v-card')).not.toBeNull());
+    let entriCards = getAllByTestId('entryCard');
+    expect(entriCards).toHaveLength(2);
   });
 });
